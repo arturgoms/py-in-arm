8af1f48d1b8f911a1473c3672cc7616d6c05e8b7
Artur Gomes <flashballclube@gmail.com>
2017-05-23 18:33:00 -0300

    Heroku things

--- Pyrm.py
+++ Pyrm.py
@@ -264,5 +264,5 @@ def verifConf():  # verifica se existe o arquivo conf.ini, se nao tiver ele cria
 
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', 1515), app)
+    http_server = WSGIServer(('0.0.0.0', 1414), app)
     http_server.serve_forever()
